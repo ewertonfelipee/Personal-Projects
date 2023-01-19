69f788d1629bb310fb3c6316238855058d4bbe91
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2023-01-19 16:09:44 -0300

    Create EnumPop.py

---
+++ EnumPop.py
@@ -0,0 +1,48 @@
+import socket, sys
+
+
+def pop_enum():
+  ip = sys.argv[1]
+  port = int(sys.argv[2])
+
+  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client.connect((ip, port))
+  #print(client)
+  banner = client.recv(1024)
+  print(banner)
+
+  client.send(b"USER xxxx\r\n")
+  client.send(b"PASS xxxx\r\n")
+
+  user = client.recv(1024)
+  passwd = client.recv(1024)
+
+  print("USER")
+  print(user)
+
+  print("PASS")
+  print(passwd)
+
+  client.send(b"STAT\r\n")
+  stat = client.recv(1024)
+  print("STAT")
+  print(stat)
+
+  client.send(b"LIST\r\n")
+  lista = client.recv(1024)
+  print("LIST")
+  print(lista)
+
+  client.send(b"QUIT\r\n")
+  quit = client.recv(1024)
+  print("QUIT")
+  print(quit)
+
+  client.close()
+  
+if len(sys.argv) < 3:
+
+  print(f"usage mode: python3 {sys.argv[0]} host port")
+  
+else:
+  pop_enum()
