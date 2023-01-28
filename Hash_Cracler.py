cd87155d9da3d554285da9699d953239f6abe12a
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2023-01-27 21:07:31 -0300

    Create Hash_Cracler.py

---
+++ Hash_Cracler.py
@@ -0,0 +1,17 @@
+import hashlib
+import pyfiglet
+
+
+banner = pyfiglet.figlet_format("Hash Cracker by Ewerton Felipe")
+print(banner)
+
+worldlist_location = str(input('Enter wordlist location: '))
+hash_input = str(input('Enter the hash type: '))
+
+with open(worldlist_location, 'r') as f:
+  for line in f.readlines():
+    hash_ob = hashlib.sha256(line.strip().encode())
+    hashed_pass = hash_ob.hexdigest()
+    if hashed_pass == hash_input:
+      print('Found cleartext password! ' + line.strip())
+      exit(0)
