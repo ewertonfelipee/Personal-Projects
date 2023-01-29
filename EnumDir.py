6155601c4c61bff2c208a067b9b04aa923b4f793
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2023-01-29 13:32:22 -0300

    Update EnumDir.py

--- EnumDir.py
+++ EnumDir.py
@@ -1,13 +1,30 @@
 import requests 
 import sys 
+import pyfiglet
 
-sub_list = open("wordlist.txt").read() 
+banner = pyfiglet.figlet_format("Directory enumerator by PR0.XY")
+print(banner)
+
+wordlist = str(input("Enter with the wordlist path: "))
+
+sub_list = open(wordlist).read() 
+
 directories = sub_list.splitlines()
 
+url = str(input("Enter with the URL: "))
+
+extension = str(input("Enter with the extension of wordlist directories: "))
+
 for dir in directories:
-    dir_enum = f"http://{sys.argv[1]}/{dir}.html" 
+
+    dir_enum = f"http://{url}/{dir}.{extension}" 
+
     r = requests.get(dir_enum)
+
     if r.status_code==404: 
+
         pass
+
     else:
-        print("Valid directory:" ,dir_enum)
+
+        print("Valid directory:", dir_enum)
