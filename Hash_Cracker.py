ed261d80849c12c5210c33c5bf9362b513b427ee
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2023-02-07 11:14:56 -0300

    Update Hash_Cracker.py

--- Hash_Cracker.py
+++ Hash_Cracker.py
@@ -6,7 +6,7 @@ banner = pyfiglet.figlet_format("Hash Cracker by Ewerton Felipe")
 print(banner)
 
 worldlist_location = str(input('Enter wordlist location: '))
-hash_input = str(input('Enter the hash type: '))
+hash_input = str(input('Enter the hash: '))
 
 with open(worldlist_location, 'r') as f:
   for line in f.readlines():
