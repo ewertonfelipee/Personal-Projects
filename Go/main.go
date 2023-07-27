03b573280b98638167fad1e06b13c63c7c84b9ba
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2023-07-27 00:00:52 -0300

    pequenas edições

--- Go/main.go
+++ Go/main.go
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-// Print "Hello World!" message
-
 package main
 import "fmt"
 
 func main() {
   fmt.Println("Hello World!")
 }
-=======
-
->>>>>>> 68ebbcca96a8e9765f56fbca471ef7f302bf4eec
